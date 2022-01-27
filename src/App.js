55793c59ea5fd1973f6fef8b5d2bd74224e681d2
Isku Esenov <snvislm2021@gmail.com>
2022-01-27 20:24:58 +0600

    login page with JWT,react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Dashboard from './components/Dashboard/dahsboard';
+import Login from './components/Login/Login';
+import Preferences from './components/Preferences/preferences';
+import useToken from './components/App/useToken';
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    const { token, setToken } = useToken();
+    
+    if(!token) {
+        return <Login setToken={setToken} />
+      }
+    return ( 
+        <div className="wrapper">
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route path="/preferences">
+            <Preferences />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
-  );
+    );
 }
 
 export default App;
